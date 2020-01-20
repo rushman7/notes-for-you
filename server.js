8454984e5f7c61e7734b020b3ff84fae480b72b3
BuildTools <arthurpisakhov1@gmail.com>
2020-01-20 15:09:33 -0500

    fixed error

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ server.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(port, () => console.log(`\n=== API running on ${PORT} ===\n`));
+server.listen(PORT, () => console.log(`\n=== API running on ${PORT} ===\n`));
